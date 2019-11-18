e09f911e5c7f60b1a5d620d574a6a469c08fc4e0
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-18 21:49:59 +0000

    Automatically backed up by Learn

--- db/migrate/20191118214315_create_movies.rb
+++ db/migrate/20191118214315_create_movies.rb
@@ -2,7 +2,10 @@ class CreateMovies < ActiveRecord::Migration[5.2]
   def change
       create_table :movies do |s|
       s.string :title
-      s.integer :
+      s.integer :release_date
+      s.string :director
+      s.string :lead
+      s.boolean :in_theater
     end
   end
 end
