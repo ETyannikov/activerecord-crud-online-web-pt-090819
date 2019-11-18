2557520d75ad647ba646f84ab49439d886fe9a4f
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-18 22:27:52 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,14 +30,13 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    binding.pry
     m.title = args[:title]
     m.release_date = args[:release_date]
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.all.first
 end
 
 def can_get_the_last_item_in_the_database
