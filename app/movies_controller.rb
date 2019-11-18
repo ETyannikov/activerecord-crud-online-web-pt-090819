e5584bc559e4ead244bca67d76c6d233b1a2a97a
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-18 22:44:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -77,7 +77,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   wat = Movie.find_by(title: "Wat?")
-  wat.update(title:"Even Awesomer Flick" )
+  wat.update(title:"Wat, huh?" )
 end
 
 def can_update_multiple_items_at_once
